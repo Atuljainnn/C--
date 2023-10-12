497c1c31beea0bfb1031e374d3ced2abc87fe592
atuljainnn <mratul3877@gmail.com>
2023-10-12 22:25:43 +0530

    pointers intro

---
+++ pointers/basic_pointer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+   int a = 5;
+   cout<<"value of a : "<< a << endl;
+   cout<<"address of a : "<< &a << endl;
+  
+
+  
+   int* ptr = &a;
+   cout<<"address store in pointer : "<<ptr << endl;
+   cout<< "Accessing value stored at position pointer pointing : " << *ptr << endl;
+   cout<<"Address of pointer : "<< &ptr << endl;
+
+  return 0;
+}
