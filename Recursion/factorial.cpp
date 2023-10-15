a1186b4ef052f26b6fba6871e9e3cd8f2b47fe7f
atuljainnn <mratul3877@gmail.com>
2023-10-15 22:54:47 +0530

    factorial by recursion

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+	if(n==1){
+		return 1;
+	}
+	if(n==0){
+		return 1;
+	}
+  int fact=n*factorial(n-1);
+	return fact;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	cout<<factorial(n);
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
