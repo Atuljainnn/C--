a82db8f788d64e2139f00d935fc74108c5bfd2cd
atuljainnn <mratul3877@gmail.com>
2024-01-27 19:27:45 +0530

    subsequences by recursion

---
+++ sub_sequences.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void sub_sequences(string str,string output, int index){
+  if(index>=str.length()){
+    cout<<"-->"<<output<<endl;
+    return;
+  }
+  
+  sub_sequences(str,output,index+1);
+  output.push_back(str[index]);
+  sub_sequences(str,output,index+1);
+  }
+int main(){
+  string str=" ";
+  cout<<"Enter the output string: ";
+  cin>>str;
+  string output=" ";
+  int index=0;
+  sub_sequences(str,output,index);
+}
