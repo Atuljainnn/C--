e66c2c3300669ae22e2f432d3b2c2e37d20e62f0
atuljainnn <mratul3877@gmail.com>
2024-01-29 23:43:24 +0530

    merge logic

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,8 +1,60 @@
 #include<iostream>
 using namespace std;
-void merge(int arr[],int s,int e){
+void merge(int arr[],int s,int e)
+{
+  int mid=(s+e)/2;
+  int lenleft=mid-s+1;
+  int lenright=e-mid;
+
+  int *left=new int[lenleft];
+  int *right=new int[lenright];
   
+  int k=s;
+
+  for(int i=0; i<lenleft;k++,i++ ){
+    int left[i]=arr[k];
+    k++;
+  }
+  k=mid+1;
+   for(int i=0; i<lenright; i++ ){
+    int right[i]=arr[k];
+    k++;
+  }
+
+  int leftindex=0;
+  int rightindex=0;
+  int mainarrayindex=s;
+
+while(leftindex<lenleft && rightindex<<lenright)
+{
+  if(arr[leftindex]<arr[rightindex]){
+    arr[mainarrayindex]=left[leftindex];
+    leftindex++;
+    mainarrayindex++;
+  }
+  else{
+    arr[mainarrayindex]=left[rightindex];
+    rightindex++;
+    mainarrayindex++;
+   }
+  }
+while(leftindex<lenleft){
+
+  arr[mainarrayindex]=left[leftindex];
+  leftindex++;
+  mainarrayindex++;
+
 }
+while(rightindex<lenright){
+
+  arr[mainarrayindex]=right[rightindex];
+  leftindex++;
+  mainarrayindex++;
+}
+delete []left;
+delete []right;
+}
+
 void mergesort(int arr[],int size,int s,int e){
   if(s>=e){
     return;
@@ -18,5 +70,8 @@ int main(){
   int s=0;
   int e=size-1;
   mergesort(arr,size,s,e);
+  for(int i=0;i<size; i++){
+    cout<<arr[i]<<" ";
+  }
   return 0;
 }
