922ccdff5d2e3634a7495895960d77a58531d6ab
atuljainnn <mratul3877@gmail.com>
2024-02-02 23:59:43 +0530

    rat in a maze

---
+++ maze.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int rat_maze(int maze[][4]){
+  
+}
+int main(){
+  int maze[4][4]={
+    {1,0,0,0},
+    {1,1,0,0},
+    {1,1,1,0},
+    {1,1,1,1}};
+
+    int row=4;
+    int col=4;
+    int srcx=0;
+    int srcy=0;
+
+  return 0;
+}
