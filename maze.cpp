d6b7b8e746727f1cd284a6e7a0b4191bf9847bd6
atuljainnn <mratul3877@gmail.com>
2024-02-05 00:00:10 +0530

    actual logic of maze

--- maze.cpp
+++ maze.cpp
@@ -1,11 +1,66 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-bool is_safe(int maze [4][4] ,int newx, int newy ){
+bool is_safe(int maze [4][4] ,int row ,int col,int newx, int newy,vector<vector<bool>>&visited ){
+  if((newx>=0 && newx<row)&&(newy>=0&&newy<col)&&
+    visited[newx][newy]==false &&
+     maze[newx][newy]==1
+   ){
+      return true;
+     }
+  else{
+  return false;
+}
 
 }
-int rat_maze(int maze[][4]){
+int rat_maze(int maze[4][4],int row,int col,int srcx,int srcy, string &output,vector<vector<bool>>&visited){
   
+
+  //Up
+  int newx=srcx+1;
+  int newy=srcy;
+  if(is_safe(maze,row,col,newx ,newy,visited)){
+    visited[newx][newy]=true;
+    output.push_back('U');
+    rat_maze(maze,row,col,newx,newy,output,visited);
+    output.pop_back();
+    visited[newx][newy]==false;
+
+  }
+  //Right
+   int newx=srcx;
+   int newy=srcy+1;
+    if(is_safe(maze,row,col,newx ,newy,visited)){
+    visited[newx][newy]=true;
+    output.push_back('R');
+    rat_maze(maze,row,col,newx,newy,output,visited);
+    output.pop_back();
+    visited[newx][newy]==false;
+
+  }
+  //Down
+   int newx=srcx-1;
+   int newy=srcy;
+    if(is_safe(maze,row,col,newx ,newy,visited)){
+    visited[newx][newy]=true;
+    output.push_back('D');
+    rat_maze(maze,row,col,newx,newy,output,visited);
+    output.pop_back();
+    visited[newx][newy]==false;
+
+  }
+  //Left
+  int newx=srcx;
+  int newy=srcy-1;
+   if(is_safe(maze,row,col,newx ,newy,visited)){
+    visited[newx][newy]=true;
+    output.push_back('L');
+    rat_maze(maze,row,col,newx,newy,output,visited);
+    output.pop_back();
+    visited[newx][newy]==false;
+
+  }
 }
 int main(){
   int maze[4][4]={
@@ -18,6 +73,9 @@ int main(){
     int col=4;
     int srcx=0;
     int srcy=0;
-
+    string output=" ";
+    vector<vector<bool>>visited(row,vector<bool>(col,false));
+  
+   rat_maze(maze,row,col,srcx,srcy,output,visited);
   return 0;
 }
