9c86fddabe096b1c3f66d4b0ccaa9872639aaebc
atuljainnn <mratul3877@gmail.com>
2024-02-13 22:22:06 +0530

    decode msg

--- mapping.cpp
+++ mapping.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
+#include<string>
+#include<vector>
+
 using namespace std;
-void mapping(string Str){
+void mapping(string &Str,string &msg ,string &ans){
+ char mapping[300]={0};
+ char start='a';
+
+  for(auto ch:Str){
+    if(ch !=' ' && mapping[ch]==0){
+    mapping[ch]=start;
+    start++;
+  }
+}
+for(auto ch:msg){
+ if(ch==' '){
+  ans.push_back('_');
+ }else{
+  char decodechar=mapping[ch];
+  ans.push_back(decodechar);
+ }
+}
 
 }
 
 int main(){
-string Str="atul";
-mapping(Str);
-return 0;
+  string Str="atul";
+  string msg="msg";
+  string Ans;
+  mapping(Str,msg,Ans);
+  return 0;
 }
