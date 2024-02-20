9986af992b74c8a0a2cfc02f3c1887a964304551
atuljainnn <mratul3877@gmail.com>
2024-02-20 23:14:08 +0530

    insertion in doubly LL

---
+++ insertion_doubly.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+class Node{
+  public:
+  int data;
+  Node* next;
+  Node* prev;
+Node(){
+  this->next=NULL;
+  this->prev=NULL;
+}
+
+Node(int data){
+  this->data=data;
+  this->next=NULL;
+  this->prev=NULL;
+}
+};
+
+
+//print
+
+void Print(Node* &head){
+  Node* temp=head;
+  while(temp!=NULL){
+    cout<<temp->data<<"->";
+    temp=temp->next;
+  }
+  cout<<endl;
+}
+
+//find length
+
+int findlength(Node* &head){
+Node* temp=head;
+int count=0;
+while(temp!=NULL){
+  count++;
+  temp=temp->next;
+}
+return count;
+}
+void insertATHead(Node* &head,Node* &tail,int data){
+   if(head==NULL){
+    Node* NewNode=new Node(data);
+   }
+ }
+
+int main(){
+  
+  Node* head=NULL;
+  Node* tail=NULL;
+  insertATHead(head,tail,5);
+  Print(head);
+
+  return 0;
+}
